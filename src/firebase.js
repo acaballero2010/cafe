21979ffdd39f9d9a54056275ced7086f220f33e7
acaballero2010 <adonis.caballero2010@yahoo.com>
2026-09-24 07:24:17 +0800

    feat: update Firebase project to pourcraft-cafe-hq under primary account

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAi9KdQAuOdyEo1T3SURKMjKJ3iVWeABb0",
-  authDomain: "sapc-intellysys-ph.firebaseapp.com",
-  projectId: "sapc-intellysys-ph",
-  storageBucket: "sapc-intellysys-ph.firebasestorage.app",
-  messagingSenderId: "923880712593",
-  appId: "1:923880712593:web:ae19bc4dce5ca69d519230"
+  apiKey: "AIzaSyDA71Tfv4-V2obLLfU1sBiA3cAdyj-yI40",
+  authDomain: "pourcraft-cafe-hq.firebaseapp.com",
+  projectId: "pourcraft-cafe-hq",
+  storageBucket: "pourcraft-cafe-hq.firebasestorage.app",
+  messagingSenderId: "102615933507",
+  appId: "1:102615933507:web:31f44081ddc7d5febeeaf6"
 }
 
 // Initialize Firebase
